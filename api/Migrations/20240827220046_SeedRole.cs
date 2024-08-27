aebdb45df70ae161a1008dc3c6807ce6c699e8f0
Jessica <jessicacarvalho.rosendo@gmail.com>
2024-08-27 18:11:00 -0400

    Changes in the Migration folder

---
+++ api/Migrations/20240827220046_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "578c170c-91b5-4eda-b247-58950923ab94", null, "Admin", "ADMIN" },
+                    { "8ae966e4-3c11-4a45-ba94-15789847386a", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "578c170c-91b5-4eda-b247-58950923ab94");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8ae966e4-3c11-4a45-ba94-15789847386a");
+        }
+    }
+}
